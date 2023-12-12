79beb5f9f2e57990f126ff266c8fcd1f01ce9ab8
timkrebs9 <timkrebs9@gmail.com>
2023-12-12 15:40:31 +0100

    added crud functions

--- app/main.py
+++ app/main.py
@@ -20,11 +20,71 @@
 # 5. Create an Endpoint to Update Users
 # 6. Create an Endpoint to Delete Users
 
+import random
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    id  : Optional[int]
+    name: str
+    age: int
+    gender: str
+    email: str
+    password: str
+
+
+# temp array to store users
+my_users = [{
+    "id" : "1",
+	"name": "Tim Krebs",
+    "age" : 16,
+    "gender": "male",
+    "email" : "timkrebs9@gmail.com",
+    "password" : "123456789"}, 
+    {
+    "id" : "2",
+    "name": "John Doe",
+    "age": "38",
+    "gender": "male",
+    "email": "johndoe@gmail.com",
+    "password": "123456789"}
+    ]
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+#######################
+# User Management #
+#######################
+#Create Users (POST)
+@app.post("/users")
+async def create_user(user: User):
+    user_dict = user.dict()
+    user_dict["id"] = random.randint(0,100000)
+    my_users.append(user_dict)
+    return {"data": user}
+
+#Read Users (GET)
+@app.get("/users/{id}")
+async def get_userbyID(id: int):
+    return {"data": my_users[id]}
+
+@app.get("/users")
+async def get_user():
+    return {"data": my_users}
+
+#Update Users (PUT, PATCH)
+@app.put("/users/{id}")
+async def update_user(id: int, user: User):
+    return {"data": {id: user}}
+
+#Delete Users (DELETE)
+@app.delete("/users/{id}")
+async def delete_user(id: int):
+    return {"data": id}
