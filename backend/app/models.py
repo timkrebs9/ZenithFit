e52e19d21212462831c085beabcf27d0d30a9f93
timkrebs9 <timkrebs9@gmail.com>
2023-12-28 12:59:50 +0100

    added new user models for crud

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,14 +5,14 @@ from sqlalchemy import Column, Integer, String
 from backend.app.database import Base
 
 
-class Users(Base):
+class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     age = Column(Integer, nullable=False)
     gender = Column(String, nullable=False)
-    email = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
