712692ae01e39978ef4d103935de523ba99484aa
timkrebs9 <timkrebs9@gmail.com>
2023-12-28 14:26:48 +0100

    refactored main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,7 @@
-import time
-import os
-from dotenv import load_dotenv
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from psycopg2 import OperationalError, DatabaseError
 from fastapi import FastAPI, HTTPException, Response, status, Depends, Request
 from sqlalchemy.orm import Session
 
 from .database import engine, SessionLocal
-from .schemas import User, UserBase
 from . import crud, models, schemas
 
 app = FastAPI()
@@ -51,51 +44,33 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)) -> dict
 
 # Read Users (GET)
 @app.get("/users", status_code=status.HTTP_200_OK, response_model=list[schemas.UserBase])
-def get_user(db: Session = Depends(get_db)) -> list:
-    return db.query(models.User).all()
+def get_user(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)) -> list:
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
 
 @app.get("/users/{user_id}", status_code=status.HTTP_200_OK, response_model=schemas.UserBase)
 async def get_user_by_id(user_id: int, db: Session = Depends(get_db)) -> dict:
-   return db.query(models.User).filter(models.User.id == user_id).first()
+   return db.query(models.User).filter(models.User.user_id == user_id).first()
 
 
 # Update Users (PUT, PATCH)
-@app.put("/users/{user_id}", status_code=status.HTTP_200_OK)
-async def update_user(user_id: int, user: schemas.UserBase, db: Session = Depends(get_db)) -> dict:
-    #cursor.execute(
-    #    """UPDATE users SET name=%s, age=%s, gender=%s, email=%s, password=%s
-    #    WHERE id=%s RETURNING * """,
-    #    (
-    #        user.name,
-    #        user.age,
-    #        user.gender,
-    #        user.email,
-    #        user.password,
-    #        str(user_id),
-    #    ),
-    #)
-    #updated_user = cursor.fetchone()
-    #conn.commit()
-    #
-    #if updated_user is None:
-    #    raise HTTPException(
-    #        status_code=404, detail=f"User not with id: {user_id} found"
-    #    )
+@app.put("/users/{user_id}", response_model=schemas.User)
+def update_user(user_id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return crud.update_user(db=db, user_id=user_id, user=user)
 
-    return {"data": "updated_user"}
 
 
 # Delete Users (DELETE)
 @app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(user_id: int, db: Session = Depends(get_db)) -> Response:
-    #cursor.execute("""DELETE FROM users WHERE id = %s RETURNING *""", (str(user_id),))
-    #deleted_user = cursor.fetchone()
-    #conn.commit()
-    #
-    #if deleted_user is None:
-    #    raise HTTPException(
-    #        status_code=404, detail=f"User not with id: {user_id} found"
-    #    )
-    #
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    crud.delete_user(db, user_id=user_id)
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)  # type: ignore
