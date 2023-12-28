794e60ae9e5fd5ef1791a053004d1c3d0edf57fe
timkrebs9 <timkrebs9@gmail.com>
2023-12-28 13:00:34 +0100

    added new user models for crud

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -2,18 +2,18 @@
 from pydantic import BaseModel
 
 
+
 class UserBase(BaseModel):
-    user_id: int | None = None
+    id: int | None = None
     name: str
     age: int
     gender: str
     email: str
     password: str
     
-    class Config:
-        orm_mode = True
         
 class UserCreate(UserBase):
-    pass
+    password: str
 
-
+class User(BaseModel):
+    email: str
