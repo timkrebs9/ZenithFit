32dde1b87b03ad6e0bf29cf9cefec5eb1fb7534e
timkrebs9 <timkrebs9@gmail.com>
2023-12-29 22:11:10 +0100

    new usercreate and post schema

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,6 +1,7 @@
+import datetime
 from pydantic import BaseModel, EmailStr
 
-
+# User schemas
 class UserBase(BaseModel):
     user_id: int | None = None
     name: str
@@ -9,16 +10,43 @@ class UserBase(BaseModel):
     email: EmailStr
     password: str
 
+    class Config:
+        orm_mode = True
 
 class UserCreate(UserBase):
-    pass  # Inherits all attributes from UserBase
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    user_id: int
+    email: EmailStr
+    created_at: datetime.datetime
+    
+    class Config:
+        orm_mode = True
 
 
 class User(BaseModel):
     email: EmailStr
 
-
 class UserUpdate(BaseModel):
     name: str | None = None
     age: int | None = None
     gender: str | None = None
+
+
+# Post schemas
+class PostBase(BaseModel):
+    post_id: int | None = None
+    title: str
+    content: str
+    published: bool
+
+class PostCreate(PostBase):
+    pass  
+
+class Post(PostBase):
+    created_at: datetime.datetime
+
+    class Config:
+        orm_mode = True
